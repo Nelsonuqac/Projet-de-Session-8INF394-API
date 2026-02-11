52be347a72e6bdfd7987c86c87b37923a6a7ae9c
NELSON <innyogwa@etu.uqac.ca>
2026-02-10 22:56:59 -0500

    Cleanup repo: ignore venv and cache

--- Tests/test_products.py
+++
@@ -1,6 +0,0 @@
-def test_get_root_products(client):
-    r = client.get("/")
-    assert r.status_code == 200
-    data = r.get_json()
-    assert "products" in data
-    assert len(data["products"]) >= 3
